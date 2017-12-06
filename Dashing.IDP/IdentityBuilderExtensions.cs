60915b0d325353ac5379bbe3dc22aa2f18dfc4b2
ksaad <kamal@dashinggroup.com.au>
2017-12-07 09:41:06 +1100

    initial

---
+++ Dashing.IDP/IdentityBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dashing.IDP
+{
+    public static class IdentityBuilderExtensions
+    {
+        //public static IIdentityServerBuilder AddMarvinUserStore(this IIdentityServerBuilder builder)
+        //{
+        //    builder.Services.AddScoped<IMarvinUserRepository, MarvinUserRepository>();
+        //    builder.AddProfileService<MarvinUserProfileService>();
+        //    return builder;
+        //}
+    }
+}
---
+++ Dashing.IDP/Startup.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Dashing.IDP
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            var connectionString = Configuration["connectionStrings:marvinUserDBConnectionString"];
+            //services.AddDbContext<MarvinUserContext>(o => o.UseSqlServer(connectionString));
+
+            //services.AddScoped<IMarvinUserRepository, MarvinUserRepository>();
+
+            var identityServerConnectionString = Configuration["connectionStrings:identityServerDataDBConnectionString"];
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
+
+            services.AddMvc();
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential();
+        }
+        public X509Certificate2 LoadCertificateFromStore(string thumbPrint)
+        {
+            using (var store = new X509Store(StoreName.My, StoreLocation.LocalMachine))
+            {
+                store.Open(OpenFlags.ReadOnly);
+                var certCollection = store.Certificates.Find(X509FindType.FindByThumbprint,
+                    thumbPrint, true);
+                if (certCollection.Count == 0)
+                {
+                    throw new Exception("The specified certificate wasn't found. Check the specified thumbprint.");
+                }
+                return certCollection[0];
+            }
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
