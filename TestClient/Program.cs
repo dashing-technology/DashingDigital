e66ea18e8456a0b235f13441b2947aad79e78f15
ksaad <kamal@dashinggroup.com.au>
2017-12-12 12:44:31 +1100

    adding api and a test client

---
+++ TestClient/Program.cs
@@ -0,0 +1,65 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace TestClient
+{
+    class Program
+    {
+        public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+        static async Task MainAsync()
+        {
+            Console.WriteLine("Please insert the secret");
+            string pass = Console.ReadLine();//secret
+
+            // discover endpoints from metadata
+
+            DiscoveryClient discoClient = new DiscoveryClient("http://localhost:5000");
+            discoClient.Policy.ValidateIssuerName = false;
+            var disco = await discoClient.GetAsync();
+
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                Console.ReadLine();
+                return;
+            }
+
+            // request token
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", pass);
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("api1");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                Console.ReadLine();
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            // call api
+            var client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            var response = await client.GetAsync("http://localhost:8080/api/identity");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
