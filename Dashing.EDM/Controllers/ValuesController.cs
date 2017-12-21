8c5d8a9ca9112ba566973a72ff1311173e262100
Kamal <kamal@dashinggroup.com.au>
2017-12-22 09:20:31 +1100

    test

--- Dashing.EDM/Controllers/ValuesController.cs
+++ Dashing.EDM/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace Dashing.EDM.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2", "smitha"};
+            return new string[] { "value1", "value2", "value3"};
         }
 
         // GET api/values/5
